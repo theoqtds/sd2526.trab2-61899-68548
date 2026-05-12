6a7cfc02f43ed2ae89fe983871a3699b03320c51
Theo da Silva <theo.qtds@gmail.com>
2026-05-12 11:52:36 +0100

    Add TLS gRPC client

--- src/sd2526/trab/impl/grpc/clients/GrpcClient.java
+++ src/sd2526/trab/impl/grpc/clients/GrpcClient.java
@@ -4,15 +4,23 @@ import static sd2526.trab.api.java.Result.error;
 import static sd2526.trab.api.java.Result.ok;
 import static sd2526.trab.api.java.Result.ErrorCode.INTERNAL_ERROR;
 
+import java.io.FileInputStream;
 import java.net.URI;
+import java.security.KeyStore;
 import java.util.function.Supplier;
 
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
 import sd2526.trab.api.java.Result;
 import sd2526.trab.api.java.Result.ErrorCode;
+
+import javax.net.ssl.TrustManagerFactory;
 
 public class GrpcClient {
 
@@ -21,8 +29,33 @@ public class GrpcClient {
 	
 	protected GrpcClient(String serverUrl) {
 		this.serverURI = URI.create(serverUrl);
-		this.channel = ManagedChannelBuilder.forAddress(serverURI.getHost(), serverURI.getPort())
-				.usePlaintext().enableRetry().build();
+
+		String trustStoreFilename = System.getProperty("javax.net.ssl.trustStore");
+		String trustStorePassword = System.getProperty("javax.net.ssl.trustStorePassword");
+
+		try {
+			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			try (FileInputStream input = new FileInputStream(trustStoreFilename)) {
+				trustStore.load(input, trustStorePassword.toCharArray());
+			}
+
+			TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+					TrustManagerFactory.getDefaultAlgorithm());
+			trustManagerFactory.init(trustStore);
+
+			SslContext context = GrpcSslContexts.configure(
+					SslContextBuilder.forClient().trustManager(trustManagerFactory)
+			).build();
+
+			this.channel = NettyChannelBuilder
+					.forAddress(serverURI.getHost(), serverURI.getPort())
+					.sslContext(context)
+					.enableRetry()
+					.build();
+
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to initialize SSL context", e);
+		}
 	}
 	
 	protected <T> Result<T> toJavaResult(Supplier<T> func) {
